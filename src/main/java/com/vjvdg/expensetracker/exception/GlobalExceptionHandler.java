bbf5100cd0deed4310865c901a23570306a3c5d0
Vincent Valena <valena.john.vincent@gmail.com>
2023-02-25 12:42:02 +0800

    Changed ControllerAdvice to RestControllerAdvice

--- src/main/java/com/vjvdg/expensetracker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/vjvdg/expensetracker/exception/GlobalExceptionHandler.java
@@ -4,18 +4,16 @@ import com.vjvdg.expensetracker.model.response.BaseResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.NestedExceptionUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    @ResponseBody
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public BaseResponse<Object> handleGenericException(Exception ex) {
         log.error("Exception: {}", ex.getMessage());
@@ -23,7 +21,6 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(HttpClientErrorException.BadRequest.class)
-    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public BaseResponse<Object> handleBadRequestException(HttpClientErrorException.BadRequest ex) {
         log.error("Exception: {}", ex.getMessage());
