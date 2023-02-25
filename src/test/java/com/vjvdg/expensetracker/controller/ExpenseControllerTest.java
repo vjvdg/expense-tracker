3812bc3b8ceb82fb2dfc22dd568ecff68156c97f
Vincent Valena <valena.john.vincent@gmail.com>
2023-02-25 12:01:46 +0800

    Added test cases

---
+++ src/test/java/com/vjvdg/expensetracker/controller/ExpenseControllerTest.java
@@ -0,0 +1,57 @@
+package com.vjvdg.expensetracker.controller;
+
+import com.vjvdg.expensetracker.dto.ExpenseDto;
+import com.vjvdg.expensetracker.model.response.BaseResponse;
+import com.vjvdg.expensetracker.service.ExpenseService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+import static com.vjvdg.expensetracker.util.TestObjectFactory.getExpenseDto;
+import static com.vjvdg.expensetracker.util.TestObjectFactory.getListOfExpenseDto;
+import static org.mockito.Mockito.any;
+
+@ExtendWith(MockitoExtension.class)
+class ExpenseControllerTest {
+
+    @Mock
+    private ExpenseService mockExpenseService;
+
+    private ExpenseController expenseController;
+
+    @BeforeEach
+    void init() {
+        expenseController = new ExpenseController(mockExpenseService);
+    }
+
+    @Test
+    void shouldReturnListOfExpenses() {
+        Mockito.when(mockExpenseService.getAllExpenses())
+                .thenReturn(getListOfExpenseDto());
+
+        BaseResponse<List<ExpenseDto>> baseResponse = expenseController.getAllExpenses();
+        List<ExpenseDto> expenses = baseResponse.getData();
+
+        Assertions.assertNotNull(expenses);
+        Assertions.assertEquals(2, expenses.size());
+    }
+
+    @Test
+    void shouldReturnOkStatusWhenSavingExpense() {
+        Mockito.doNothing().when(mockExpenseService).saveExpense(any(ExpenseDto.class));
+
+        BaseResponse<Object> baseResponse = expenseController.saveExpense(getExpenseDto());
+        HttpStatus status = baseResponse.getStatus();
+
+        Assertions.assertNotNull(baseResponse);
+        Assertions.assertEquals(HttpStatus.OK, status);
+    }
+
+}
---
+++ src/test/java/com/vjvdg/expensetracker/service/ExpenseServiceTest.java
@@ -0,0 +1,54 @@
+package com.vjvdg.expensetracker.service;
+
+import com.vjvdg.expensetracker.dto.ExpenseDto;
+import com.vjvdg.expensetracker.entity.ExpenseEntity;
+import com.vjvdg.expensetracker.repository.ExpenseEntityRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static com.vjvdg.expensetracker.util.TestObjectFactory.getExpenseDto;
+import static com.vjvdg.expensetracker.util.TestObjectFactory.getExpenseEntity;
+import static com.vjvdg.expensetracker.util.TestObjectFactory.getListOfExpenseEntity;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+class ExpenseServiceTest {
+
+    @Mock
+    private ExpenseEntityRepository mockExpenseEntityRepo;
+
+    private ExpenseService expenseService;
+
+    @BeforeEach
+    void init() {
+        expenseService = new ExpenseService(mockExpenseEntityRepo);
+    }
+
+    @Test
+    void shouldReturnListOfExpenses() {
+        Mockito.when(mockExpenseEntityRepo.findAll())
+                .thenReturn(getListOfExpenseEntity());
+
+        List<ExpenseDto> expenseDtoList = expenseService.getAllExpenses();
+
+        Assertions.assertNotNull(expenseDtoList);
+        Assertions.assertEquals(2, expenseDtoList.size());
+    }
+
+    @Test
+    void shouldSaveExpenseEntity() {
+        Mockito.when(mockExpenseEntityRepo.save(any(ExpenseEntity.class)))
+                        .thenReturn(getExpenseEntity());
+        expenseService.saveExpense(getExpenseDto());
+
+        Mockito.verify(mockExpenseEntityRepo, times(1)).save(any(ExpenseEntity.class));
+    }
+}
